2f1bec8378f6d94064011b0e7145aa60b5e51975
Jonatan <jonathanfds2007@gmail.com>
2024-08-16 18:16:03 -0300

    refactor(urls)adicionado path editar, update, delete

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import home, editar, update, delete
 
 urlpatterns = [
-    path('', home)
+    path('', home, name="home"),
+    path('editar/<int:id>', editar, name='editar'),
+    path('update/<int:id>', update, name='update'),
+    path('delete/<int:id>', delete, name='delete'),
 ]    
