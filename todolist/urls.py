845765a0e464a6319daab99d264d76c7f399ba8a
Rodigo G Martins <mrgobetti0@gmail.com>
2024-08-22 17:25:12 -0300

    feat(implementação): Criação dos details das tasks e URL's de redirecionamento

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, editar, update, delete
+from .views import home, editar, update, delete, TaskDetailView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path('', home, name="home"),
     path('editar/<int:id>', editar, name='editar'),
     path('update/<int:id>', update, name='update'),
     path('delete/<int:id>', delete, name='delete'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='task_update'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]    
--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,10 @@
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, UpdateView, DeleteView
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from .models import Meta
-from .forms import MetaForm
+from .forms import MetaForm, TaskForm
 
 
 @login_required
@@ -57,3 +59,37 @@ def delete(request, id):
 
 # READ, DELETE, UPDATE - Task
 # Details meta/task;
+
+#READ: Detalhes de uma tarefa específica:
+class TaskDetailView(DetailView):
+    # Exibe os detalhes de uma tarefa específica.
+    # Usa DetailView, que já cuida de toda a lógica de exibição de detalhes.
+    # template_name: Especifica o template que será usado para exibir os detalhes da tarefa.
+    # context_object_name: O nome do objeto que será passado ao template.
+    model = Task
+    template_name = 'task_detail.html'
+    context_object_name = 'task'
+
+#UPDATE: Atualizar uma tarefa existente:
+class TaskUpdateView(UpdateView):
+    # Permite atualizar uma tarefa existente.
+    # Usa UpdateView, que cuida da lógica de edição e atualização de um objeto.
+    # fields: Especifica quais campos da Task serão exibidos no formulário de atualização.
+    # get_success_url: Redireciona para a página de detalhes da tarefa após a atualização bem-sucedida.
+    model = Task
+    fields = ['name', 'meta', 'description', 'is_active', 'user']
+    template_name = 'task_form.html'
+    context_object_name = 'task'
+
+    def get_success_url(self):
+        return reverse_lazy('task_detail', kwargs={'pk': self.object.pk})
+
+#DELETE: Excluir uma tarefa
+class TaskDeleteView(DeleteView):
+    # Permite excluir uma tarefa.
+    # Usa DeleteView, que já cuida de toda a lógica de exclusão.
+    # success_url: Redireciona para uma lista de tarefas após a exclusão bem-sucedida.
+    model = Task
+    template_name = 'Task_confirm_delete.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('task_list')
